4b179a25e7c1d40f7b4929671308e55ebb4770c4
Eric-Schecter <eric199002@icloud.com>
2023-01-30 17:03:04 +0800

    update comment for sum shader

--- src/App/reduce/shader/sum.fs
+++ src/App/reduce/shader/sum.fs
@@ -11,6 +11,8 @@ void main(){
     vec2 uv=gl_FragCoord.xy/u_resolution;
     vec2 texelSize = 1./(u_resolution*2.);
     
+    // https://github.com/regl-project/regl/blob/gh-pages/example/reduction.js
+    // todo: still don't know why here should use minus 
     vec4 self=texture(u_buffer,(uv-vec2(0.,0.)*texelSize));
     vec4 right=texture(u_buffer,(uv-vec2(1.,0.)*texelSize));
     vec4 bottom=texture(u_buffer,(uv-vec2(0.,1.)*texelSize));
